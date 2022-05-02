b7907b8c31a2f7998f757361487dc84742249bcf
NenadDeWeerdt <91470272+NenadDeWeerdt@users.noreply.github.com>
2022-05-02 15:26:10 +0200

    Update register.php

--- register.php
+++ register.php
@@ -1,32 +1,3 @@
-<?php
-session_start();
-
-    include "php_library/connection.php";
-
-    // $database = new Database();
-    // $database->createConnection();
-
-    $firstname = $_POST["firstname"];
-    $lastname = $_POST["lastname"];
-    $email = $_POST["email"];
-    $birthdate = $_POST["birthdate"];
-    $gender = $_POST["gender"];
-    $password = $_POST["password"];
-    $Confpassword = $_POST["Confpassword"];
-
-    $sqlGet = "select * from users;";
-    $User = $database->getQuery($sqlGet);
-    $result = mysqli_query($conn, $sqlGet);
-    $resultCheck = mysqli_num_rows($result);
-    
-    // $sqlInsert = "INSERT INTO users (firstname, lastname, email, birthdate, gender, password ) values ('$firstname', '$lastname', '$email', '$bithdate', '$gender','$password')";
-    // $database->InsertQuery($sqlInsert)
-
-    if ($password == $Confpassword) {
-        
-    }
-    
-?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -36,16 +7,21 @@ session_start();
         <title>Register</title>
         <link rel="stylesheet" href="assets/css/register.css">
     </head>
+
     <body>
+    <?php
+        include "db.php"; 
+        $conn = create_connection();
+    ?>
             <div class="space">
             <h1>Register</h1>
-            <form method="POST" action="">
+            <form method="POST" action="add.php">
             <div>
-                <label for="form-firstname">
+                <label for="firstname">
                     Firstname
                 </label>
                 <br>
-                <input type="text" name="firstname" id="form-firstname" value="<?php echo $_POST['firstname']; ?>" required>
+                <input type="text" name="firstname" id="firstname" required>
             </div>
             <br>
             <div>
@@ -53,7 +29,7 @@ session_start();
                     Lastname
                 </label>
                 <br>
-                <input type="text" name="lastname" id="form-lastname" value="<?php echo $_POST['lastname']; ?>" required>
+                <input type="text" name="lastname" id="form-lastname" required>
             </div>
             <br>
             <div>
@@ -61,7 +37,7 @@ session_start();
                     email
                 </label>
                 <br>
-                <input type="text" name="email" id="form-email" value="<?php echo $_POST['email']; ?>"required>
+                <input type="text" name="email" id="form-email" required>
             </div>
             <br>
             <div class="form-Birthdate">
@@ -69,25 +45,19 @@ session_start();
                     Birthdate
                 </label>
                 <br>
-                    <input type="date" name="birthdate" value="<?php echo $_POST['birthdate']; ?>"required>
+                    <input type="date" name="birthdate" required>
             </div>
             <br>
             <div>
-                <label for="form-pref-gender" value="<?php echo $_POST['gender']; ?>">
+                <label for="form-pref-gender">
                     Gender
                 </label>
 
                 <br>
-                <select name="pref-gender" id="form-pref-gender">
-                    <option value="male">
-                        Male
-                    </option>
-                    <option value="Female">
-                        Female
-                    </option>
-                    <option value="x">
-                        x
-                    </option>
+                <select name="taskOption">
+                    <option value="male">male</option>
+                    <option value="female">female</option>
+                    <option value="x">x</option>
                 </select>
             </div>
             <br>
@@ -96,7 +66,7 @@ session_start();
                     Password
                 </label>
                 <br>
-                <input type="password" name="password" id="form-password value="<?php echo $_POST['password']; ?>"" required>
+                <input type="password" name="password" id="form-password" required>
             </div>
             <br>
             <div>
@@ -104,11 +74,11 @@ session_start();
                     Confirm password
                 </label>
                 <br>
-                <input type="password" name="Confpassword" id="form-Confpassword" value="<?php echo $_POST['Confpassword']; ?>" required>
+                <input type="password" name="Confpassword" id="form-Confpassword" required>
             </div>
             </div>
             <br>
-            <button class="button" id="button-accComplete" onclick="location.href='index.php'">Create Account</button>
+            <button class="button" id="button-accComplete">Create Account</button>
             <br>
             <br>
             <a href="login.php" id="alreadyAccount" >Already have an account? Click here!</a>
