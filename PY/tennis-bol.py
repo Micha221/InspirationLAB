83e013fa6d62845590708b90eb37622c57cefd4b
SarahBDM <91462266+SarahBDM@users.noreply.github.com>
2022-05-08 21:03:03 +0200

    Add files via upload

---
+++ PY/tennis-bol.py
@@ -0,0 +1,44 @@
+# %%
+import pandas as pd
+import numpy as np
+import requests
+from bs4 import BeautifulSoup
+
+# %%
+producten_alle_paginas = []
+
+for i in range(1,150):
+    response = requests.get(f"https://www.bol.com/be/nl/s/?searchtext=tennis")
+    content = response.content
+    parser = BeautifulSoup(content, 'html.parser')
+    body = parser.body
+    producten = body.find_all(class_="product-item--row js_item_root")
+    producten_alle_paginas.extend(producten)
+    
+len(producten_alle_paginas)
+
+# %%
+prijs = float(producten_alle_paginas[5].meta.get('content'))
+productnaam = producten_alle_paginas[5].find(class_="product-title--inline").a.getText()
+print(prijs)
+print(productnaam)
+
+# %%
+productlijst = []
+
+for item in producten_alle_paginas:
+    if item.find(class_="product-prices").getText() == '\nNiet leverbaar\n':
+        prijs = None
+    else:
+        prijs = float(item.meta['content'])
+    product = item.find(class_="product-title--inline").a.getText()
+    productlijst.append([product, prijs])
+    
+print(productlijst[:150])
+
+# %%
+df = pd.DataFrame(productlijst, columns=["Product", "prijs"])
+print(df.shape)
+df["prijs"].describe()
+
+
