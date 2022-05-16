2f5b369951b6375ff3afe634b6b84c69da68eb1e
Sander03 <79938960+Sander03@users.noreply.github.com>
2022-05-16 14:31:10 +0200

    Add files via upload

---
+++ assets/js/registratie.js
@@ -0,0 +1,12 @@
+var ButtonElement = document.querySelector("#button-accComplete");
+var keyword = document.querySelector("#password").value;
+var Confkeyword = document.querySelector("#Confpassword").value;
+
+if(keyword !=""){console.log(keyword);}
+// console.log(Confkeyword);
+
+// if (keyword != Confkeyword) {
+//     alert("Password do not match")
+//     window.history.back();
+// } else {
+    
---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+var RegisterButton = document.querySelector("#register-button")
+var LoginButton = document.querySelector("#login-button")
+
+RegisterButton.addEventListener("click", function(){
+    
+})
