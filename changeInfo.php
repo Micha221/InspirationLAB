301debbd1c77e4d484b4561e26688791cb9277a4
NenadDeWeerdt <91470272+NenadDeWeerdt@users.noreply.github.com>
2022-05-21 16:22:37 +0200

    Update changeInfo.php

--- changeInfo.php
+++ changeInfo.php
@@ -15,22 +15,30 @@
                 include "db.php"; 
                 session_start();
                 $conn = create_connection();
+                $id = $_SESSION['User_ID'];
+                $sql = "select * from users where User_ID = '$id'";
+                $result = mysqli_query($conn, $sql);
+                $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
                 $firstname =  $_POST["firstname"];
                 $lastname = $_POST["lastname"];
                 $email = $_POST["email"];
                 $password = $_POST["password"];
+                $curPassword = $row['keyword'];
+                $newPassword = $_POST["newPassword"];
                 $birthdate = $_POST["birthdate"];
-                $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+                $newPasswordHashed = password_hash($newPassword, PASSWORD_DEFAULT);
+                $sqlInsert = "UPDATE users SET firstname = '$firstname', lastname= '$lastname',keyword = '$newPasswordHashed', email = '$email', birthdate = '$birthdate' WHERE User_ID = '$id';";
+                if(password_verify($password,$row['keyword'])){
+                    if(mysqli_query($conn, $sqlInsert)){
+                        echo "<h1><center>Account information succesfully changed</h1>";}
+                   else{echo"Something went wrong on our side :/";}
+                }else{echo "<h1><center>Password incorrect</h1>"; }
                 $id = $_SESSION['User_ID'];
                 $_SESSION['firstname'] = $firstname;
                 $_SESSION['lastname'] = $lastname;
                 $_SESSION['birthdate'] = $birthdate;
                 $_SESSION['email'] = $email;
 
-                $sqlInsert = "UPDATE users SET firstname = '$firstname', lastname= '$lastname', email = '$email', birthdate = '$birthdate' WHERE User_ID = '$id';";
-                 if(mysqli_query($conn, $sqlInsert)){
-                     echo "<h1><center>Account information succesfully changed</h1>";}
-                else{echo"Something went wrong on our side :/";}
                 ?>
             <br>
             <br>
