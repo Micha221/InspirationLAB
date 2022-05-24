018538fafb4d7d161807ee514b7cd1be15969d45
NenadDeWeerdt <91470272+NenadDeWeerdt@users.noreply.github.com>
2022-05-24 16:08:34 +0200

    Update index.php

--- index.php
+++ index.php
@@ -28,8 +28,7 @@
             <!-- <button class="button" id="OverzichtButton" onclick="window.location.href='Overzicht.php'">Overzicht</button> -->
             <button class="button" id="HelpButton"onclick="window.location.href='help.php'">Help</button>
             <button class="adminButton" id="adminButton" onclick="window.location.href='admin.php'">Admin</button>
-            <button class="button" id="AddEventButton" onclick=openForm()>Add Event</button>
-            <button class="button" id="AddPersonButton"onclick="window.location.href='AddPerson.php'">Add Person</button>
+            <button class="button" id="AddEventButton"onclick=openForm()>Add Event</button>
             <button class="button" id="ListAddedPersons" onclick="window.location.href='ListAddedPersons.php'">Added Persons</button>
             <button id="InstellingenButton" onclick="window.location.href='settings.php'">Settings</button>
             <button id="logoutButton" onclick="window.location.href='logout.php'">Log out</button>
