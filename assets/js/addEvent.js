1fce64aaa0a92446c838cb2b463164c5af659060
NenadDeWeerdt <91470272+NenadDeWeerdt@users.noreply.github.com>
2022-06-17 14:40:05 +0200

    Add files via upload

---
+++ assets/js/addEvent.js
@@ -0,0 +1,12 @@
+const events = [];
+
+function createEvent(startDate, title, endDate) {
+  const event = {
+    id: 1, // You must use a custom id generator
+    title: title,
+    start: startDate,
+    allDay: endDate ? endDate : true // If there's no end date, the event will be all day of start date
+  }
+
+  events.push(event);
+}
